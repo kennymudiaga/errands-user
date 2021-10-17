5ab32e0bfc99f4c4abed6d5eff9cb355851c7bdc
Ese Kenneth Mudiaga <kenesecorp@hotmail.com>
2021-10-17 05:55:16 +0100

    cleanup signup command : remove commented code

--- FireRepository/AccountCommand.cs
+++ FireRepository/AccountCommand.cs
@@ -1,13 +1,8 @@
 ﻿using Errands.Users.Domain.Commands;
 using Errands.Users.Domain.Entities;
-using Errands.Users.Domain.Exceptions;
 using Errands.Users.Domain.Models;
 using Google.Cloud.Firestore;
 using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FireRepository
@@ -27,20 +22,8 @@ namespace FireRepository
         public async Task<LoginResponse> SignUp(SignUpRequest model)
         {
             var account = new Account(model);
-            var accountDoc = accountsCollection.Document(account.Id);
-            //await accountDoc.CreateAsync(new
-            //{
-            //    account.Name,
-            //    account.Email,
-            //    account.Username,
-            //    account.Phone,
-            //    account.DateCreated,
-            //    account.LastLogin,
-            //    account.Roles
-            //});
-            var data = account.ToDictionary();            
-            var result = await accountDoc.CreateAsync(data);
-            
+            var accountDoc = accountsCollection.Document(account.Id);            
+            _ = await accountDoc.CreateAsync(account.ToDictionary());
             return new LoginResponse
             {
                 Email = account.Email,
